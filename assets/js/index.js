0177cbd1f113c126d16bdc0e93f449f683319aa5
Andrey Halan <halan.andrey@gmail.com>
2017-05-31 15:09:44 +0300

    ticket:3 added landing page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,21 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from 'react-bootstrap/lib/Button';
+import Panel from 'react-bootstrap/lib/Panel';
 
-var Hello = React.createClass ({
-    render: function() {
+class Landing extends React.Component {
+    render() {
         return (
-            <h1>
-            Hello, React!!!
-            </h1>
-        )
+            <Panel bsStyle="default" className="main-menu-wrapper text-center">
+                <Button bsStyle="default" className="btn-landing" href="#">
+                    Deposit DASH to Ripple
+                </Button>
+                <Button bsStyle="default" className="btn-landing" href="#">
+                    Withdraw DASH from Ripple
+                </Button>
+            </Panel>
+        );
     }
-})
+}
 
-ReactDOM.render(<Hello />, document.getElementById('container'))
+ReactDOM.render(<Landing />, document.getElementById('container'));
