c99ca22bb1e6b45fe9dd2233abe2a6d902761282
Andrey Halan <halan.andrey@gmail.com>
2017-05-31 21:57:22 +0300

    ticket:4 added page component to react

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,69 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Hello = React.createClass ({
-    render: function() {
-        return (
-            <h1>
-            Hello, React!!!
-            </h1>
-        )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slug: '/'
+        };
     }
-})
 
-ReactDOM.render(<Hello />, document.getElementById('container'))
+    render() {
+        let slug = 'about';
+
+        if (slug === '/') {
+            return (<Landing />);
+        } else {
+            return (<Page slug={slug}/>);
+        }
+    }
+}
+
+class Landing extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: null
+        };
+    }
+
+    loadPage() {
+        $.ajax({
+            url: this.props.slug,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({page: JSON.parse(data)[0]});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(status, err);
+            }.bind(this)
+        });
+    }
+
+    componentDidMount() {
+        this.loadPage();
+    }
+
+    render() {
+        let page = this.state.page,
+            pageTempate = <p>Page does not exists</p>;
+
+        if (page !== null) {
+            pageTempate = (
+                <div dangerouslySetInnerHTML={{__html: page.fields.description}}/>
+            )
+        }
+        return pageTempate;
+    }
+}
+
+const container = document.getElementById('container');
+ReactDOM.render(<App />, container);
