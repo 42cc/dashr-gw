0ae0ba37f5267c3624275ed6a7671742bde7488f
Andrey Halan <halan.andrey@gmail.com>
2017-06-01 14:22:30 +0300

    ticket:4 added page cms

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from ckeditor.widgets import CKEditorWidget
+
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from apps.core.models import Page
+
+
+class PageAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Page
+        fields = '__all__'
+
+
+class PageAdmin(admin.ModelAdmin):
+    form = PageAdminForm
+
+
+admin.site.register(Page, PageAdmin)
--- gateway/settings.py
+++ gateway/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'compressor',
     'webpack_loader',
+    'ckeditor',
 
     'apps.core',
 ]
