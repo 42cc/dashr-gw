30856a07fc0a30c537f977dc0390931ee439e22a
Andrey Halan <halan.andrey@gmail.com>
2017-06-01 18:41:17 +0300

    ticket:4 flake8

--- gateway/urls.py
+++ gateway/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[a-z0-9-]+?)/$', GetPageDetailsView.as_view(), name='page'),
+    url(r'^(?P<slug>[a-z0-9-]+?)/$',
+        GetPageDetailsView.as_view(), name='page'),
 ]
