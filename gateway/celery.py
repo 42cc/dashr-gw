7f94eac1feec38df5ac2de907bdae9f03d266b21
Illia Volochii <illia.volochii@gmail.com>
2017-09-01 16:49:02 +0300

    ticket:18 Configure Celery

---
+++ gateway/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gateway.settings')
+
+app = Celery('gateway')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+}
