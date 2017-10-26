97c0a3547099daf1370dbeae8b3e6fde40bd48a2
Illia Volochii <illia.volochii@gmail.com>
2017-10-26 18:42:31 +0300

    ticket:8 Create a form `DepositTransactionModelForm()`

---
+++ apps/core/tests/test_forms.py
@@ -0,0 +1,30 @@
+from mock import patch
+
+from django.test import TestCase
+
+from apps.core import forms
+from apps.core.models import DepositTransaction
+
+
+class DepositTransactionModelFormTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.form = forms.DepositTransactionModelForm
+        cls.valid_form = cls.form(
+            data={'ripple_address': 'rp2PaYDxVwDvaZVLEQv7bHhoFQEyX1mEx7'},
+        )
+
+    def test_form_without_data(self):
+        self.assertFalse(self.form(data={}).is_valid())
+
+    def test_form_with_invalid_ripple_address(self):
+        form = self.form(data={'ripple_address': 'Invalid address'})
+        self.assertFalse(form.is_valid())
+        self.assertIn('ripple_address', form.errors)
+        self.assertIn(
+            'The Ripple address is not valid.',
+            form.errors['ripple_address'],
+        )
+
+    def test_form_with_valid_ripple_address(self):
+        self.assertTrue(self.valid_form.is_valid())
