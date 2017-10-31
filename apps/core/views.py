a9791c63bef5c801169b9ea20d6202998366128b
Illia Volochii <illia.volochii@gmail.com>
2017-10-31 17:03:13 +0200

    ticket:8 Bind a form to the page '/deposit/'

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.forms.models import model_to_dict
-from django.views.generic.base import TemplateView, View
+from django.views.generic import TemplateView, View
+from django.views.generic.edit import FormMixin
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from .forms import DepositTransactionModelForm
 from .models import Page
 
 
@@ -33,3 +35,32 @@ class GetPageDetailsView(View):
 
             return JsonResponse(ctx, safe=False)
         return render(request, 'base.html')
+
+
+class DepositSubmitApiView(View, FormMixin):
+    form_class = DepositTransactionModelForm
+
+    def post(self, request):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        transaction = form.save()
+        return JsonResponse(
+            {
+                'success': True,
+                'dash_wallet': transaction.dash_address,
+                'status_url': '/status/{}/'.format(transaction.id),
+            },
+        )
+
+    def form_invalid(self, form):
+        return JsonResponse(
+            {
+                'success': False,
+                'ripple_address_error': form.errors['ripple_address'][0],
+            },
+        )
--- gateway/urls.py
+++ gateway/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
-
-from apps.core.views import IndexView, GetPageDetailsView
+from apps.core.views import (
+    DepositSubmitApiView,
+    GetPageDetailsView,
+    IndexView,
+)
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^submit-deposit/$',
+        DepositSubmitApiView.as_view(), name='submit-deposit'),
     url(r'^(?P<slug>[a-z0-9-]+?)/$',
         GetPageDetailsView.as_view(), name='page'),
     url(r'^(?P<slug>[a-z0-9-]+?)/how-to/$',
