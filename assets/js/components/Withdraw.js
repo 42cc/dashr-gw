b4ba38b887fd3a89ca98c1aeace1b443507d2b09
Illia Volochii <illia.volochii@gmail.com>
2017-12-21 16:48:39 +0200

    ticket:12 Bind `WithdrawalSubmitApiView()` to an appropriate component

--- assets/js/components/Withdraw.js
+++ assets/js/components/Withdraw.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DjangoCSRFToken from 'django-react-csrftoken';
 import Button from 'react-bootstrap/lib/Button';
 import Panel from 'react-bootstrap/lib/Panel';
 import Form from 'react-bootstrap/lib/Form';
@@ -11,23 +12,41 @@ import InputGroup from 'react-bootstrap/lib/InputGroup';
 
 export default class DepositDash extends React.Component {
     render() {
-        const title = (
-            <h3>Withdraw DASH from Ripple</h3>
-        )
+        if (this.state && this.state.rippleAddress && this.state.statusUrl) {
+            return (
+                <Panel bsStyle="default" className="panel-wrapper panel-wrapper-container">
+                    <Col sm={12} md={6}>
+                        <b>You have initiated a transaction from Ripple to Dash</b>
+                        <p>
+                            <span>Please transfer your Ripple tokens to this address:</span>
+                            {' '}
+                            <b>{this.state.rippleAddress}</b>
+                        </p>
+                        <p>
+                            You can track status of this transaction
+                            {' '}
+                            <a href={this.state.statusUrl}>here</a>.
+                        </p>
+                    </Col>
+                </Panel>
+            )
+        }
         return (
-            <Panel header={title} bsStyle="default"
+            <Panel header="Withdraw DASH from Ripple" bsStyle="default"
                    className="panel-wrapper panel-wrapper-container">
                 <Col sm={12} md={6}>
-                    <Form>
-                        <FormGroup controlId="id_ripple_address">
+                    <Form onSubmit={this.handleFormSubmit.bind(this)} id="withdrawal-form">
+                        <DjangoCSRFToken/>
+                        <FormGroup controlId="id_dash_address">
                             <Col md={12}>
                                 <ControlLabel>Enter your dash address, please:</ControlLabel>
                                 <InputGroup>
-                                    <FormControl type="text"/>
+                                    <FormControl type="text" name="dash_address"/>
                                     <InputGroup.Button>
                                         <Button type="submit">Start</Button>
                                     </InputGroup.Button>
                                 </InputGroup>
+                                {this.getDashAddressError()}
                                 <FormControl.Feedback />
                                 <HelpBlock>
                                     <Button bsStyle="link" href="/withdraw/how-to/">Need help?</Button>
@@ -38,5 +57,33 @@ export default class DepositDash extends React.Component {
                 </Col>
             </Panel>
         );
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+        $('button[type="submit"]').prop('disabled', true);
+        $.post(
+            urls.submitWithdrawal,
+            $('#withdrawal-form').serialize(),
+        ).done((data) => {
+            if (data.success) {
+                this.setState({
+                    rippleAddress: data['ripple_address'],
+                    destinationTag: data['destination_tag'],
+                    statusUrl: data['status_url'],
+                });
+            } else {
+                $('#deposit-form .form-group').addClass('has-error');
+                this.setState({dashAddressError: data['dash_address_error']});
+            }
+        }).always(() => {$('button[type="submit"]').prop('disabled', false);})
+    }
+
+    getDashAddressError() {
+        if (this.state && this.state.dashAddressError) {
+            return (
+                <HelpBlock>{this.state.dashAddressError}</HelpBlock>
+            );
+        }
     }
 }
