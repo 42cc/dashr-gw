e252224471e3f9454aab92971fa19d243d3d7e0e
Illia Volochii <illia.volochii@gmail.com>
2018-01-04 17:10:14 +0200

    ticket:15 Use values from settings to compute amounts

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -1,12 +1,16 @@
 from decimal import Decimal, ROUND_DOWN, ROUND_UP
 
+from django.apps import apps
+
 dash_minimal = Decimal('0.00000001')
 
 
 def get_minimal_withdrawal_amount():
-    gateway_fee = Decimal('0.005')
-    miner_fee = Decimal('0.001')
-    minimal_amount = (dash_minimal + miner_fee) / (1 - gateway_fee)
+    gateway_settings = apps.get_model('core', 'GatewaySettings').get_solo()
+    gateway_fee = gateway_settings.gateway_fee_percent / 100
+    minimal_amount = (
+        (dash_minimal + gateway_settings.dash_miner_fee) / (1 - gateway_fee)
+    )
     minimal_amount = minimal_amount.quantize(
         dash_minimal,
         rounding=ROUND_UP,
@@ -21,11 +25,13 @@ def get_received_amount_dash(amount):
         rounding=ROUND_DOWN,
     )
 
-    gateway_fee = Decimal('0.005')
-    miner_fee = Decimal('0.001')
+    gateway_settings = apps.get_model('core', 'GatewaySettings').get_solo()
+    gateway_fee = gateway_settings.gateway_fee_percent / 100
 
     # Subtract fees.
-    received_amount = amount * (1 - gateway_fee) - miner_fee
+    received_amount = (
+        amount * (1 - gateway_fee) - gateway_settings.dash_miner_fee
+    )
 
     # Round received amount to 8 decimal places.
     received_amount = received_amount.quantize(
